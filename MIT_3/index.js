cf0467a34745fc516509d7fbf704585f43082f5c
HEMA-KUMAR10 <94049064+HEMA-KUMAR10@users.noreply.github.com>
2024-04-29 13:02:11 +0530

    Add files via upload

---
+++ MIT_3/index.js
@@ -0,0 +1,13 @@
+var firstName = document.getElementById("first-name").value;
+var lastName = document.getElementById("last-name").value;
+var email = document.getElementById("email").value;
+var pass = document.getElementById("password").value;
+
+var button = document.getElementById("login-button").addEventListener("click",function(){
+    check();
+});
+
+var fieldOne = document.getElementById("field1");
+
+
+
